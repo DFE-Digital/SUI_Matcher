aa47f2e4112307212a50bd5d2b593746da247891
kristan66 <kristan@hotmail.co.uk>
2025-03-13 16:48:43 +0000

    SUI-75: Fake nhs fhir api and data generation enhancements for demo

---
+++ SUI.Test.FakeNhsFhirApi/FakeItem.cs
@@ -0,0 +1,9 @@
+﻿namespace SUI.Test.FakeNhsFhirApi;
+
+public class FakeItem
+{
+    public string MatchType { get; set; }
+    public FakePerson Person { get; set; } = new();
+    public string ResponseJson { get; set; }
+    public double Score { get; set; }
+}
---
+++ SUI.Test.FakeNhsFhirApi/FakePerson.cs
@@ -0,0 +1,26 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace SUI.Test.FakeNhsFhirApi;
+
+public class FakePerson
+{
+    [Name("DOB")]
+    public string Dob { get; set; }
+
+    [Name("Email")]
+    public string Email { get; set; }
+
+    [Name("Surname")]
+    public string Family { get; set; }
+
+    [Name("Gender")]
+    public string Gender { get; set; }
+
+    [Name("GivenName")]
+    public string Given { get; set; }
+    [Ignore]
+    public string NhsId { get; set; }
+
+    [Name("Phone")]
+    public string Phone { get; set; }
+}
