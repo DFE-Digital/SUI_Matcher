1eadf59423f8825e3e27957ec8a01febccb20c9d
Rilwan Owolabi <rilforge@rilwans-mbp.home>
2025-03-26 14:14:40 +0000

    SUI-99 Created client to parse Wigan data txt files continuously

--- SUI.Test/Integration/TestContextLoggerProvider.cs
+++ SUI.Test/Integration/TestContextLoggerProvider.cs
@@ -5,13 +5,15 @@ namespace SUI.Test.Integration;
 public class TestContextLoggerProvider : ILoggerProvider
 {
     private readonly TestContext _testContext;
+    private readonly List<string> _logMessages;
 
-    public TestContextLoggerProvider(TestContext testContext)
+    public TestContextLoggerProvider(TestContext testContext, List<string>? logMessages = null)
     {
         _testContext = testContext;
+        _logMessages = logMessages ?? new();
     }
 
-    public ILogger CreateLogger(string categoryName) => new TestContextLogger(_testContext, categoryName);
+    public ILogger CreateLogger(string categoryName) => new TestContextLogger(_testContext, categoryName, _logMessages);
 
     public void Dispose() { }
 
@@ -19,11 +21,13 @@ public class TestContextLoggerProvider : ILoggerProvider
     {
         private readonly TestContext _testContext;
         private readonly string _categoryName;
+        private readonly List<string> _logMessages;
 
-        public TestContextLogger(TestContext testContext, string categoryName)
+        public TestContextLogger(TestContext testContext, string categoryName, List<string>? logMessages = null)
         {
             _testContext = testContext;
             _categoryName = categoryName;
+            _logMessages = logMessages ?? new();
         }
 
         public IDisposable? BeginScope<TState>(TState state) => null;
@@ -33,6 +37,8 @@ public class TestContextLoggerProvider : ILoggerProvider
         public void Log<TState>(Microsoft.Extensions.Logging.LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
         {
             _testContext.WriteLine($"[{logLevel}] {_categoryName}: {formatter(state, exception)}");
+            
+            _logMessages.Add($"{formatter(state, exception)}");
         }
     }
 }
