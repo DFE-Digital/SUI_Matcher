efe22286635890247cb2d17a38f1348dd6b7f453
Joshua Taylor MBCS <joshua.taylor@education.gov.uk>
2025-11-18 15:19:29 +0000

    chore: Ensure string code is logged

--- src/external-api/Services/NhsFhirClient.cs
+++ src/external-api/Services/NhsFhirClient.cs
@@ -82,7 +82,7 @@ public class NhsFhirClient(IFhirClientFactory fhirClientFactory, ILogger<NhsFhir
             }
 
             logger.LogInformation("Patient record found for Nhs number");
-            logger.LogInformation("Security flags {SecurityFlag}", string.Join(',', data.Meta?.Security ?? []));
+            logger.LogInformation("Security flags {SecurityFlag}", string.Join(',', data.Meta?.Security.Select(c => c.Code) ?? []));
             return new DemographicResult
             {
                 Result = new NhsPerson
