7f08b084c5393c9ef1b883ac8b728ecfc0148739
Stuart.Maskell <stuart.maskell@hippodigital.co.uk>
2026-02-18 09:39:57 +0000

    feat: add support for version 2 in SearchStrategy4

--- src/matching-api/Search/SearchStrategy4.cs
+++ src/matching-api/Search/SearchStrategy4.cs
@@ -11,7 +11,7 @@ namespace MatchingApi.Search;
 public class SearchStrategy4 : ISearchStrategy
 {
     private int AlgorithmVersion { get; }
-    private static readonly IReadOnlyCollection<int?> AllVersions = [1];
+    private static readonly IReadOnlyCollection<int?> AllVersions = [1, 2];
 
     public SearchStrategy4(int? version = null)
     {
@@ -33,6 +33,7 @@ public class SearchStrategy4 : ISearchStrategy
         return version switch
         {
             1 => Version1(queryBuilder),
+            2 => Version2(queryBuilder),
             _ => throw new ArgumentOutOfRangeException(nameof(version), $"Unsupported version: {version}")
         };
     }
@@ -50,6 +51,30 @@ public class SearchStrategy4 : ISearchStrategy
         queryBuilder.AddNonFuzzyGfdRangePostcode(usePostcodeWildcard: false);
 
         queryBuilder.AddFuzzyGfdRange();
+
+        queryBuilder.AddFuzzyGfdRangePostcode();
+
+        return queryBuilder.Build();
+    }
+    
+    /// <summary>
+    ///  Version 2 removes the non-fuzzy and fuzzy GFD range searches compared to V1, to test the impact of removing these searches on matching performance
+    /// </summary>
+    /// <param name="queryBuilder"></param>
+    /// <returns></returns>
+    private static OrderedDictionary<string, SearchQuery> Version2(SearchQueryBuilder queryBuilder)
+    {
+        queryBuilder.AddNonFuzzyGfd();
+
+        queryBuilder.AddFuzzyGfd();
+
+        queryBuilder.AddFuzzyAll();
+
+        // queryBuilder.AddNonFuzzyGfdRange(); // Removal is intentional to test impact of removing this search
+
+        queryBuilder.AddNonFuzzyGfdRangePostcode(usePostcodeWildcard: false);
+
+        // queryBuilder.AddFuzzyGfdRange(); // Removal is intentional to test impact of removing this search
 
         queryBuilder.AddFuzzyGfdRangePostcode();
 
