3b130028776ca91f781641d4fb8e8eb79fd37e99
Stuart.Maskell <stuart.maskell@hippodigital.co.uk>
2026-03-20 10:37:43 +0000

    chore: remove unused comment in ps1 file

--- run-tests-local.ps1
+++ run-tests-local.ps1
@@ -1,3 +1,1 @@
-# dotnet test --settings tests.runsettings --collect:"XPlat Code Coverage" && reportgenerator -reports:"**/coverage.cobertura.xml" -targetdir:"coveragereport" -reporttypes:"Html" && open coverage-report/index.html
-#
 dotnet test --settings tests.runsettings --collect:"XPlat Code Coverage" && reportgenerator -reports:"**/coverage.cobertura.xml" -targetdir:"coveragereport" -reporttypes:"Html" && open coverage-report/index.html
