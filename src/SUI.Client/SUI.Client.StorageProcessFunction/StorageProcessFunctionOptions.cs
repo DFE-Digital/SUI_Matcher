0099086d18ec17cef1c76d0454b97302c26145c0
Stuart.Maskell <stuart.maskell@hippodigital.co.uk>
2026-04-08 10:33:56 +0100

    update StorageProcessFunctionOptions.cs

--- src/SUI.Client/SUI.Client.StorageProcessFunction/StorageProcessFunctionOptions.cs
+++ src/SUI.Client/SUI.Client.StorageProcessFunction/StorageProcessFunctionOptions.cs
@@ -6,12 +6,11 @@ public sealed class StorageProcessFunctionOptions
 {
     public const string SectionName = "StorageProcessFunction";
 
-    public string QueueName { get; set; } = "storage-process-job";
+    public string QueueName { get; init; } = "storage-process-job";
 
     public string ProcessedContainerName { get; init; } = "processed";
-    public string? MatchApiBaseAddress { get; set; }
-    public string SearchStrategy { get; set; } =
+    public string? MatchApiBaseAddress { get; init; }
+    public string SearchStrategy { get; init; } =
         SharedConstants.SearchStrategy.Strategies.Strategy4;
-    public int? StrategyVersion { get; set; } = 2;
-    public int MaxRequestsPerSecond { get; set; } = 10;
+    public int? StrategyVersion { get; init; } = 2;
 }
