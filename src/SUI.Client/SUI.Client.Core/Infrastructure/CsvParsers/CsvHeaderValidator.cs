94774b37fb0a3595ccff5ee453fd5043fe6438b1
Stuart.Maskell <stuart.maskell@hippodigital.co.uk>
2026-04-21 11:47:29 +0100

    make CsvHeaderValidator.cs pure
    No hard coded values

--- src/SUI.Client/SUI.Client.Core/Infrastructure/CsvParsers/CsvHeaderValidator.cs
+++ src/SUI.Client/SUI.Client.Core/Infrastructure/CsvParsers/CsvHeaderValidator.cs
@@ -2,22 +2,10 @@ namespace SUI.Client.Core.Infrastructure.CsvParsers;
 
 public static class CsvHeaderValidator
 {
-    public static void Validate(HashSet<string> headers, string parserType)
+    public static void Validate(HashSet<string> headers, IEnumerable<string> requiredHeaders)
     {
-        var requiredHeaders = parserType switch
-        {
-            CsvParserNameConstants.TypeOne => new[]
-            {
-                "Id",
-                "GivenName",
-                "FamilyName",
-                "DOB",
-                "Postcode",
-            },
-            _ => throw new InvalidOperationException($"Unknown parser type: {parserType}."),
-        };
-
         var missingHeaders = requiredHeaders.Where(x => !headers.Contains(x)).ToArray();
+
         if (missingHeaders.Length > 0)
         {
             throw new InvalidOperationException(
