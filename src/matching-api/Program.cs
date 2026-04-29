91994b2cf5e8a01da6a7f53f273ad39693aa7877
Stuart.Maskell <stuart.maskell@hippodigital.co.uk>
2026-04-29 11:18:53 +0100

    remove audit log files and references in program.cs

--- src/matching-api/Program.cs
+++ src/matching-api/Program.cs
@@ -1,5 +1,4 @@
 using System.Text.Json.Serialization;
-using System.Threading.Channels;
 using DotNetEnv;
 using MatchingApi;
 using MatchingApi.Services;
@@ -52,19 +51,6 @@ builder.Services.AddSingleton<IActivityHashService, ActivityHashService>();
 builder.Services.AddSingleton<IValidationService, ValidationService>();
 builder.Services.AddSingleton<INhsFhirClient, NhsFhirClientApiWrapper>();
 builder.Services.AddSingleton<IReconciliationService, ReconciliationService>();
-
-// Audit logging setup
-builder.Services.AddFeatureManagement();
-var auditFeatureFlag = builder.Configuration.GetValue<bool>("FeatureManagement:EnableAuditLogging");
-Console.WriteLine($"[AUDIT] Matching API started with audit logging set to {auditFeatureFlag}");
-if (auditFeatureFlag)
-{
-    builder.AddAzureTableServiceClient("tables");
-    builder.Services.AddHostedService<AuditLogBackgroundService>();
-}
-
-builder.Services.AddSingleton(Channel.CreateUnbounded<AuditLogEntry>());
-builder.Services.AddSingleton<IAuditLogger, ChannelAuditLogger>();
 
 IHttpClientBuilder client = builder.Services.AddHttpClient<INhsFhirClient, NhsFhirClientApiWrapper>(
     static client => client.BaseAddress = new Uri("https+http://external-api")
